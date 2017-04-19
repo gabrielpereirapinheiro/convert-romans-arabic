ab97fdb5e27442703e9408355f33a670208d2b80
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-04-19 16:52:28 -0300

    Com testes e mk

---
+++ src/funcoes.cpp
@@ -0,0 +1,142 @@
+/*TRABALHO DE METODOS DE PROGRAMACAO
+*
+* ALUNO : GABRIEL PEREIRA PINHEIRO
+* MATRICULA: 14/0020764
+* UNIVERSIDADE DE BRASILIA
+* DEPARTAMENTO DE CIENCIA DA COMPUTACAO
+* PROFESSOR: JAN MENDONCA
+*/
+
+#include <ctype.h>
+#include "bibliotecas.hpp"
+
+int converte_valor(char letra)
+{
+
+	int valor_retornado;
+
+	switch(letra)
+	{
+		case 'I':
+			valor_retornado = 1;
+			break;
+
+		case 'V':
+			valor_retornado = 5;
+			break;
+				
+		case 'X':
+			valor_retornado = 10;
+			break;
+
+		case 'L':
+			valor_retornado = 50;
+			break;	
+
+		case 'C':
+			valor_retornado = 100;
+			break;	
+
+		case 'D':
+			valor_retornado = 500;
+			break;		
+
+		case 'M':
+			valor_retornado = 1000;
+			break;
+
+		default :
+			valor_retornado=-1;	
+	}
+
+	return valor_retornado;
+}
+void transfere_string_int(char const *string,int *vetor,int size)
+{
+	int i;
+
+	for (int i = 0; i <  size; ++i)
+    {
+    	vetor[i] = converte_valor(string[i]);
+    }
+}
+
+int converte(char const *string)
+{
+    int vetor[30];
+    int valor;
+    int tamanho;
+    int total;
+
+ 	
+    tamanho = strlen (string);
+
+    transfere_string_int(string,vetor,tamanho);
+
+    total = valor_total(vetor,tamanho);
+    
+    return total;
+}
+
+int valor_total(int *vetor,int tamanho)
+{
+	int i,total=0,invalido=0,passado=0,repetido=0;
+
+    for (int i = 0; i < tamanho; ++i)
+    {
+    	if(vetor[i]<vetor[i+1] && i+1<tamanho && vetor[i] != -1)
+    	{
+    		if(vetor[i] == passado)
+    		{
+    			printf(" S %d \n",repetido+1 );
+    			repetido++;
+    		}
+    		total = (vetor[i+1] - vetor[i]) + total;
+    		i = i + 1;
+    	}
+    	else
+    	{    		
+    		total = total + vetor[i] ;
+    	}
+    	if (passado == vetor[i])
+    	{
+    		repetido++;
+    		printf("A%d\n", repetido);
+    	}
+    	
+    	if(repetido!=0 && repetido!=3)
+    	{
+    		if (vetor[i]!=passado)
+    		{
+    			repetido=0;
+    		}
+    	}
+
+    	if(vetor[i]==-1 || repetido==3)
+    	{
+            printf("IMM");
+    		invalido=1;
+            break;
+    	}
+    	passado = vetor[i];
+    	
+    }
+    if(invalido!=0)
+    {
+    	total=-1;
+    }
+
+    return total;
+}
+
+void mostra_resultados(int total, char *string,int tamanho )
+{
+	if(total!=-1)
+	{
+		printf("O numero romano %s convertido é igual a %d \n",string,total );
+	}
+	else
+	{
+		printf("Numero inválido !\n");
+	}
+}
---
+++ src/lab.cpp
@@ -0,0 +1,23 @@
+/*TRABALHO DE METODOS DE PROGRAMACAO
+*
+* ALUNO : GABRIEL PEREIRA PINHEIRO
+* MATRICULA: 14/0020764
+* UNIVERSIDADE DE BRASILIA
+* DEPARTAMENTO DE CIENCIA DA COMPUTACAO
+* PROFESSOR: JAN MENDONCA
+*/
+
+#include "bibliotecas.hpp"
+#include "../include/catch.hpp"
+
+TEST_CASE("TESTE","teste")
+{
+	SECTION("SESSAO TESTE")
+	{
+		REQUIRE(converte("XL")==40);
+		REQUIRE(converte("DXXVII")==527);
+		REQUIRE(converte("xx")==-1);
+		REQUIRE(converte("XXXX")==-1);
+	}
+}
+
