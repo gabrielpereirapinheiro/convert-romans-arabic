d09d6fbc1a86df57a27412742cf3e7d4feb04fea
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-04-19 16:54:18 -0300

    outras pastas

--- include/bibliotecas.hpp
+++ include/bibliotecas.hpp
@@ -19,3 +19,5 @@ void transfere_string_int(char *string,int *vetor,int size);
 int valor_total(int *vetor,int tamanho);
 
 void mostra_resultados(int total, char *string,int tamanho );
+
+int converte(char const *string);
